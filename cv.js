88d3da7232f58a9a731ee4cfa74719e54634286e
smancke <s.mancke@tarent.de>
2014-02-26 08:55:23 +0100

    start of skill management

--- cv.js
+++ cv.js
@@ -1,54 +1,48 @@
 var cv = angular.module('cv', []);
 
-cv.directive('cvShortField', function() {
-  return {
-      restrict: 'EA',
-      require: 'ngModel',
-      scope: {
-          'ngModel': '=',
-          'label': '@',
-      },
-      template: '<div class="form-group">\
-                  <label for="postalcode" class="col-lg-4 control-label">{{label}}</label>\
-                  <div class="col-lg-5">\
-                   <input type="text" class="form-control" id="postalcode" ng-model="ngModel">\
-                  </div>\
-                 </div>'
-  }
-});
+$( "#all-items-box" ).sortable({
+//$( "#all-items-box, #beginner-box, #advanced-box, #expert-box" ).sortable({
+//    connectWith: ".connectedSortable"
+});//.disableSelection();
 
-cv.directive('contenteditable', function() {
-    return {
-        require: 'ngModel',
-        link: function(scope, elm, attrs, ctrl) {
-            
-            // model -> view
-            ctrl.$render = function() {
-                elm.html(ctrl.$viewValue);
-            };
+//$( "#all-items-box, #beginner-box, #advanced-box, #expert-box" ).droppable({
+//    drop: function(event, ui) {
+//    }
+//});
 
-            /**
-             * handling the maxlength parameter
-             */
-            elm.bind('keyup', function(event) {
-                scope.$apply(function() {
-                    ctrl.$setViewValue(elm.html());
-                });
-            });
+
+cv.controller('SkillCtrl', ['$scope', '$http', function($scope, $http) {
+
+    $scope.ratingItems = [];
+    $scope.selection = 'prog';
+
+    $scope.byCategory = function(category) {
+        return function(item) {
+            return item.category == category;
         }
     }
-});
 
+    $http.get('/tech-rating/api/default/ratingitem')
+        .success(function(data, status, headers, config) {
+            $scope.ratingItems = data;
+        })
+        .error(function(data, status, headers, config) {
+            alert("error while loading "+status);
+        });
+    
+        
+}]);
 
 cv.controller('CvCtrl', ['$scope', '$http', function($scope, $http) {
 
     $scope.modified = false;
     $scope.cv =  { 
+        'educations': [ {} ],
         'jobs': [
             {},
-            { 'start': '2011-01',
-              'end': '2011-02',
-              'type': 'Freelance',
+            { 'start': {'year' : '2011', 'month': '01'},
+              'end':  {'year' : '2013', 'month': '05'},
+              'type': 'freelance',
               'company': 'tarent GmbH, Bonn',
               'project': 'OSIAM',
               'projectDescription': 'Erstellung eines Open Source Identity und Access Management Systems auf Basis von SCIM und oAuth2.0',
@@ -58,7 +52,7 @@ cv.controller('CvCtrl', ['$scope', '$http', function($scope, $http) {
             },
             { 'start': '2011-01',
               'end': '2011-02',
-              'type': 'Freelance',
+              'type': 'freelance',
               'company': 'tarent GmbH, Bonn',
               'project': 'OSIAM',
               'projectDescription': 'Erstellung eines Open Source Identity und Access Management Systems auf Basis von SCIM und oAuth2.0',
@@ -70,37 +64,33 @@ cv.controller('CvCtrl', ['$scope', '$http', function($scope, $http) {
     };
     $scope.ignoreNextWatch = false;
 
-    $scope.addJob = function() {
-        $scope.cv.jobs.unshift({});
+    $scope.addEducation = function() {
+        $scope.cv.educations.unshift({});
     }
 
-    $scope.deleteJob = function(job) {
+    $scope.deleteFromCollection = function(collection, item) {
         var pos = -1;
-        for (var i=0; i<$scope.cv.jobs.length; i++) {
-            if (job == $scope.cv[i]) {
+        for (var i=0; i<collection.length; i++) {
+            if (job == collection[i]) {
                 pos = i;
                 break;
             }
         }
         if (pos != -1) {
-            $scope.cv.jobs.splice(pos, 1);
+            collection.splice(pos, 1);
         }            
     }
 
-    $scope.loadUri = function(uri) {
-        $http.get(uri)
-            .success(function(data, status, headers, config) {
-                $scope.cv = data;
-                $scope.modified = false;
-            })
-            .error(function(data, status, headers, config) {
-                alert("error while loading "+status);
-            });
+    $scope.deleteEducation = function(education) {
+        $scope.deleteFromCollection($scope.cv.educations, education);
     }
 
-    var cvuri = location.search.split('cvuri=')[1];
-    if (cvuri) {
-        $scope.loadUri(cvuri);
+    $scope.addJob = function() {
+        $scope.cv.jobs.unshift({});
+    }
+
+    $scope.deleteJob = function(job) {
+        $scope.deleteFromCollection($scope.cv.jobs, education);
     }
         
     $scope.$watch('cv', function(newValue, oldValue) {
@@ -112,6 +102,17 @@ cv.controller('CvCtrl', ['$scope', '$http', function($scope, $http) {
             $scope.modified = true;
         }
     }, true);        
+
+    $scope.loadUri = function(uri) {
+        $http.get(uri)
+            .success(function(data, status, headers, config) {
+                $scope.cv = data;
+                $scope.modified = false;
+            })
+            .error(function(data, status, headers, config) {
+                alert("error while loading "+status);
+            });
+    }
 
     $scope.save = function() {
         if ($scope.cv.cvURI) { // update
@@ -135,3 +136,101 @@ cv.controller('CvCtrl', ['$scope', '$http', function($scope, $http) {
         }
     };
 }]);
+
+
+cv.directive('cvShortField', function() {
+  return {
+      restrict: 'E',
+      require: 'ngModel',
+      scope: {
+          'ngModel': '=',
+          'label': '@',
+          'input-id': '@',
+      },
+      template: '<div class="form-group">\
+                  <label for="{{input-id}}" class="col-sm-2 control-label">{{label}}</label>\
+                  <div class="col-sm-5">\
+                   <input type="text" class="form-control input-sm" id=""{{input-id}}" ng-model="ngModel">\
+                  </div>\
+                 </div>'
+  }
+});
+
+cv.directive('cvCeFormRow', function() {
+  return {
+      restrict: 'E',
+      require: 'ngModel',
+      scope: {
+          'label': '@',
+          'fontStyle': '@',
+          'ngModel': '=',
+      },
+      template: '<div class="row" style="margin-bottom: 1px;">\
+                      <div class="col-sm-3" style="text-align: right;">{{label}}</div>\
+                      <div class="col-sm-9" contenteditable ng-model="ngModel" style="{{fontStyle}} min-height: 24px; max-width: 450px; overflow-wrap: break-word; word-wrap: break-word;"></div>\
+                    </div>'
+  }
+});
+
+function generateOptionYearList() {
+    var out = '';
+    for (var i=new Date().getFullYear(); i>=1980; i--) {
+        out += '                          <option>' + i + '</option>\n';
+    }
+    return out;
+}
+cv.directive('cvDateField', function() {
+    return {
+      restrict: 'E',
+      require: 'ngModel',
+      scope: {
+          'ngModel': '=',
+      },
+      template: '<div class="row" style="max-width: 170px;">\
+                      <div class="col-sm-5" style="padding-right: 0px;">\
+                        <select class="col-sm-6 form-control input-sm"  ng-model="ngModel.month">\
+                          <option>01</option>\
+                          <option>02</option>\
+                          <option>03</option>\
+                          <option>04</option>\
+                          <option>05</option>\
+                          <option>06</option>\
+                          <option>07</option>\
+                          <option>08</option>\
+                          <option>09</option>\
+                          <option>10</option>\
+                          <option>11</option>\
+                          <option>12</option>\
+                        </select>\
+                      </div>\
+                      <div class="col-sm-7" style="padding-left: 0px;">\
+                        <select class="form-control input-sm" style="margin: 0px;" ng-model="ngModel.year">\
+          '+ generateOptionYearList() +' \
+                        </select>\
+                      </div>\
+                    </div>'
+  }
+});
+
+
+cv.directive('contenteditable', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            
+            // model -> view
+            ctrl.$render = function() {
+                elm.html(ctrl.$viewValue);
+            };
+
+            /**
+             * handling the maxlength parameter
+             */
+            elm.bind('keyup', function(event) {
+                scope.$apply(function() {
+                    ctrl.$setViewValue(elm.html());
+                });
+            });
+        }
+    }
+});
--- cv.php
+++ cv.php
@@ -8,6 +8,7 @@
     <link href="lib/bootstrap.css" rel="stylesheet" media="screen"/>
 
     <script src="lib/jquery.min.js"></script>
+    <script src="lib/ui/jquery-ui.js"></script>
     <script src="lib/bootstrap.min.js"></script>
     <script src="lib/angular.min.js"></script>
 
@@ -19,15 +20,16 @@
      <h1>Profil {{cv.givenName}} {{cv.familyName}}</h1>
       <blockquote>
         <p><small>Bitte beachten: Alle Angaben sind freiwillig. Mit dem Ausf&uuml;llen der 
-            Daten best&auml;tigen Sie, dass sie damit Einverstanden diese Daten der tarent zur Nutzung zu überlassen.
-            Die Nutzung beinhaltet sowohl die interne Veranrbeitung in digitaler und nicht digitaler Form,
-            sowie die Weiterreichung an (potenzielle) Kunden und Geschäftspartner der tarent.</small></p>
+            Daten best&auml;tigen Sie, dass sie damit Einverstanden diese Daten der tarent GmbH zur Nutzung zu überlassen.
+            Die Nutzung beinhaltet sowohl die interne Verarbeitung in digitaler und nicht digitaler Form,
+            sowie die Weiterreichung an (potenzielle) Kunden und Geschäftspartner der tarent GmbH.</small></p>
       </blockquote>
 
-      <div> <!-- navigation -->
+      <!-- Navigation -->
+      <div>
         <a href="index.html"
            class="btn btn-default btn-md">
-          <span class="glyphicon glyphicon-chevron-left"></span> Übersicht
+          <span class="glyphicon glyphicon-chevron-left"></span> &Uuml;ersicht
         </a>
         <button ng-click="save()" type="button" class="btn btn-default btn-md">
           <span ng-if="modified" class="glyphicon glyphicon-floppy-save text-danger"></span>
@@ -37,6 +39,76 @@
       </div>
       <br/>
 
+
+      <!-- Skills -->
+      <div ng-controller="SkillCtrl">
+        <ul class="nav nav-pills">
+          <li class="active"><a href="#" ng-click="selection = 'prog'" data-toggle="tab">Programmierung</a></li>
+          <li><a href="#" ng-click="selection = 'tooling'" data-toggle="tab">Tools</a></li>
+          <li><a href="#" ng-click="selection = 'test'" data-toggle="tab">Testing</a></li>
+          <li><a href="#" ng-click="selection = 'paradigm'" data-toggle="tab">Konzepte</a></li>
+        </ul>
+        <div class="panel panel-default">
+          <div id="all-items-box" class="connectedSortable panel-body">
+            <div ng-repeat="item in ratingItems | filter:byCategory(selection)"
+                 style="display: inline-block; margin: 4px; padding: 2px; padding-left: 6px; padding-right: 6px; border: solid 1px #cccccc; border-radius: 3px;">{{item.name}} <small><span style="color: #cccccc" class="glyphicon glyphicon-remove"></div>
+            <div contenteditable="true" style="display: inline-block;  border: solid 1px #cccccc;"></div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+
+        <div class="col-lg-4">
+          <div class="panel panel-default" style="min-height: 300px">
+            <div class="panel-heading clearfix">
+              <span class="panel-title"><strong>Anfänger</strong></span>
+              <span class="input-group pull-right" style="width: 180px;">
+                <input type="text" class="input-sm form-control">
+                <span class="input-group-btn">
+                  <button class="input-sm btn btn-default" type="button">Hinzu</button>
+                </span>
+              </span>
+            </div>
+            <div class="connectedSortable panel-body" id="beginner-box">
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4">
+          <div class="panel panel-default"  style="min-height: 300px">
+            <div class="panel-heading clearfix">
+              <span class="panel-title"><strong>Fortgeschritten</strong></span>
+              <span class="input-group pull-right" style="width: 180px;">
+                <input type="text" class="input-sm form-control">
+                <span class="input-group-btn">
+                  <button class="input-sm btn btn-default" type="button">Hinzu</button>
+                </span>
+              </span>
+            </div>
+            <div class="connectedSortable panel-body" id="advanced-box">
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4">
+          <div class="panel panel-default"  style="min-height: 300px">
+            <div class="panel-heading clearfix">
+              <span class="panel-title"><strong>Experte</strong></span>
+              <span class="input-group pull-right" style="width: 180px;">
+                <input type="text" class="input-sm form-control">
+                <span class="input-group-btn">
+                  <button class="input-sm btn btn-default" type="button">Hinzu</button>
+                </span>
+              </span>
+            </div>
+            <div class="connectedSortable panel-body" id="expert-box">
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <!-- Allgemeine Daten -->
       <div class="panel panel-default">
         <div class="panel-heading">
           <h3 class="panel-title">Allgemeine Daten</h3>
@@ -46,132 +118,158 @@
           <form class="form-horizontal" role="form">
             
             <cv-short-field ng-model="cv.givenName" input-id="givenName" label="Vorname"></cv-short-field>
-
             <cv-short-field ng-model="cv.familyName" input-id="familyName" label="Nachname"></cv-short-field>
-
+            <hr/>
             <cv-short-field ng-model="cv.placeOfBirth" input-id="placeOfBirth" label="Geburtsort-/Land"></cv-short-field>
-
+            <cv-short-field ng-model="cv.languages" input-id="languages" label="Sprachen"></cv-short-field>
             <cv-short-field ng-model="cv.familyStatus" input-id="familyStatus" label="Familienstand/Kinder"></cv-short-field>
+            <cv-short-field ng-model="cv.interests" input-id="interests" label="Interessen/Hobbys"></cv-short-field>
+          </form>
+        </div>   
+      </div>
 
+      <!-- Kontaktinfomationen -->
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Kontaktinfomationen</h3>
+        </div>
+        <div class="panel-body">
+
+          <form class="form-horizontal" role="form">
+            <cv-short-field ng-model="cv.email" input-id="email" label="E-Mail"></cv-short-field>
+            <cv-short-field ng-model="cv.phoneMobile" input-id="phoneMobile" label="Telefon (Mobil)"></cv-short-field>
+            <cv-short-field ng-model="cv.phoneFixed" input-id="phoneFixed" label="Telefon (Festnetz)"></cv-short-field>
+            <cv-short-field ng-model="cv.xingProfile" input-id="xingProfile" label="Xing Profil"></cv-short-field>
+
+            <hr/>
             <cv-short-field ng-model="cv.streetAddress" input-id="streetAddress" label="Stra&szlig;e, Hausnummer"></cv-short-field>
-
             <div class="form-group">
-              <label for="postalcode" class="col-lg-4 control-label">PLZ/Ort</label>
-              <div class="col-lg-2">
-                <input type="text" class="form-control" id="postalcode" ng-model="cv.postalcode">
+              <label for="postalcode" class="col-sm-2 control-label">PLZ/Ort</label>
+              <div class="col-sm-2">
+                <input type="text" class="form-control input-sm" id="postalcode" ng-model="cv.postalcode">
               </div>
-              <div class="col-lg-3">
-                <input type="text" class="form-control" id="locality" ng-model="cv.locality">
+              <div class="col-sm-3">
+                <input type="text" class="form-control input-sm" id="locality" ng-model="cv.locality">
               </div>
             </div>
           </form>
         </div>   
       </div>
 
-
+      <!-- Ausbildung -->
       <div class="panel panel-default">
         <div class="panel-heading">
           <div>
-            <span class="panel-title"><strong>Projekt & Berufserfahrung</strong></span>
-            <button ng-click="addJob()" type="button" class="btn btn-default btn-xs pull-right">
-              <span class="glyphicon glyphicon-plus"></span> Neu
+            <span class="panel-title"><strong>Ausbildung</strong></span>
+            <button ng-click="addEducation()" type="button" class="btn btn-default btn-xs pull-right">
+              <span class="glyphicon glyphicon-plus"></span> Ausbildung Einfügen
             </button>
           </div>
-        </div>
-
+        </div>      
         <div class="panel-body">
-          <table class="table table-striped">
-            <thead>
-              <tr>
-                <th></th>
-                <th>von</th>
-                <th>bis</th>
-                <th>Typ</th>
-                <th>Beschreibung</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr ng-repeat="job in cv.jobs">
-                <td><button ng-click="deleteJob()" type="button" class="btn btn-default btn-xs pull-right">
-                    <span class="glyphicon glyphicon-trash"></span>
+          <form class="form-horizontal" role="form">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>von</th>
+                  <th>bis</th>
+                  <th>Beschreibung</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr ng-repeat="education in cv.educations">
+                  <td width="40"><button ng-click="deleteEducation(education)" type="button" class="btn btn-default btn-xs pull-right">
+                      <span class="glyphicon glyphicon-trash"></span>
                   </button></td>
-                <td>{{job.start}}</td>
-                <td>{{job.end}}</td>
-                <td>{{job.type}}</td>
-                <td>
-                  <strong><div contenteditable ng-model="job.company"></div></strong>
-                  <strong><div contenteditable ng-model="job.project"></div></strong>
-                  <div contenteditable ng-model="job.projectDescription"></div>
-                  <div contenteditable ng-model="job.role"></div>
-                  <div contenteditable ng-model="job.activity"></div>
-                  <div contenteditable ng-model="job.topics"></div>
-                </td>
-              </tr>
-            </tbody>
-          </table>          
+                  <td width="150">
+                    <cv-date-field ng-model="education.start"></cv-date-field>
+                  </td>
+                  <td width="150">
+                    <cv-date-field ng-model="education.end"></cv-date-field>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="row" style="margin-bottom: 1px;">
+                      <div class="col-sm-3" style="text-align: right;">Typ</div>
+                      <div class="col-sm-3">
+                        <select class="form-control input-sm" ng-model="education.type">     
+                          <option value="school">Schule</option>
+                          <option value="education">Ausbildung</option>
+                          <option value="university">Uni/FH</option>
+                          <option value="training">Fortbildung/Schulung</option>
+                          <option value="researchProjects">Besondere Studienarbeit</option>
+                          <option value="other">Sonstiges</option>
+                        </select>
+                      </div>
+                    </div>
+                    <cv-ce-form-row label="Title" ng-model="education.title" font-style="font-weight: bold;"></cv-ce-form-row>
+                    <cv-ce-form-row label="Beschreibung" ng-model="education.description"></cv-ce-form-row>
+                  </td>
+                </tr>
+              </tbody>
+            </table>          
+          </form>
         </div>
       </div>
 
 
 
-
+      <!-- Projekt & Berufserfahrung -->
       <div class="panel panel-default">
         <div class="panel-heading">
           <div>
             <span class="panel-title"><strong>Projekt & Berufserfahrung</strong></span>
             <button ng-click="addJob()" type="button" class="btn btn-default btn-xs pull-right">
-              <span class="glyphicon glyphicon-plus"></span> Neu
+              <span class="glyphicon glyphicon-plus"></span> Projekt/Berufserfahrung einfügen
             </button>
           </div>
         </div>      
         <div class="panel-body">
-          <table class="table table-striped">
-            <thead>
-              <tr>
-                <th></th>
-                <th>von</th>
-                <th>bis</th>
-                <th>Typ</th>
-                <th>Beschreibung</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr ng-repeat="job in cv.jobs">
-                <td><button ng-click="deleteJob(job)" type="button" class="btn btn-default btn-xs pull-right">
-                    <span class="glyphicon glyphicon-trash"></span>
+          <form class="form-horizontal" role="form">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>von</th>
+                  <th>bis</th>
+                  <th>Beschreibung</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr ng-repeat="job in cv.jobs">
+                  <td width="40"><button ng-click="deleteJob(job)" type="button" class="btn btn-default btn-xs pull-right">
+                      <span class="glyphicon glyphicon-trash"></span>
                   </button></td>
-                <td>{{job.start}}</td>
-                <td>{{job.end}}</td>
-                <td>{{job.type}}</td>
-                <td>
-                  <div class="row" style="margin-bottom: 1px;">
-                    <div class="col-lg-3" style="text-align: right;">Firma</div>
-                    <strong><div class="col-lg-9 bg-warningXX" contenteditable ng-model="job.company" style="min-height: 24px; max-width: 450px;"></div></strong>
-                  </div>
-                  <div class="row" style="margin-bottom: 1px;">
-                    <div class="col-lg-3" style="text-align: right;">Projekt</div>
-                    <strong><div  class="col-lg-9 bg-warningXX" contenteditable ng-model="job.project" style="min-height: 24px; max-width: 450px;"></div></strong>
-                  </div>
-                  <div class="row" style="margin-bottom: 1px;">
-                    <div class="col-lg-3" style="text-align: right;">Beschreibung</div>
-                    <div  class="col-lg-9 bg-warningXX" contenteditable ng-model="job.projectDescription" style="min-height: 24px; max-width: 450px;"></div>
-                  </div>
-                  <div class="row" style="margin-bottom: 1px;">
-                    <div class="col-lg-3" style="text-align: right;">Rolle</div>
-                    <div  class="col-lg-9 bg-warningXX" contenteditable ng-model="job.role" style="min-height: 24px; max-width: 450px;"></div>
-                  </div>
-                  <div class="row" style="margin-bottom: 1px;">
-                    <div class="col-lg-3" style="text-align: right;">Aktivitäten</div>
-                    <div  class="col-lg-9 bg-warningXX" contenteditable ng-model="job.activity" style="min-height: 24px; max-width: 450px;"></div>
-                  </div>
-                  <div class="row" style="margin-bottom: 1px;">
-                    <div class="col-lg-3" style="text-align: right;">Topics/Technologien</div>
-                    <div  class="col-lg-9 bg-warningXX" contenteditable ng-model="job.topics" style="min-height: 24px; max-width: 450px;"></div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>          
+                  <td width="150">
+                    <cv-date-field ng-model="job.start"></cv-date-field>
+                  </td>
+                  <td width="150">
+                    <cv-date-field ng-model="job.end"></cv-date-field>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="row" style="margin-bottom: 1px;">
+                      <div class="col-sm-3" style="text-align: right;">Typ</div>
+                      <div class="col-sm-3">
+                        <select class="form-control input-sm" ng-model="job.type">     
+                          <option value="employment">Anstellung</option>
+                          <option value="employmedProject">Projekt als Angestellter</option>
+                          <option value="company">Unternemerische T&auml;tigkeit</option>
+                          <option value="freelance">Projekt als Freelancer</option>
+                        </select>
+                      </div>
+                    </div>
+                    <cv-ce-form-row label="Firma, Ort" ng-model="job.company" font-style="font-weight: bold;"></cv-ce-form-row>
+                    <cv-ce-form-row label="Projekt" ng-model="job.project" font-style="font-weight: bold;"></cv-ce-form-row>
+                    <cv-ce-form-row label="Beschreibung" ng-model="job.projectDescription"></cv-ce-form-row>
+                    <cv-ce-form-row label="Rolle, Aufgaben" ng-model="job.role"></cv-ce-form-row>
+                    <cv-ce-form-row label="Topics/Technologien" ng-model="job.topics"></cv-ce-form-row>
+                  </td>
+                </tr>
+              </tbody>
+            </table>          
+          </form>
         </div>
       </div>
 
