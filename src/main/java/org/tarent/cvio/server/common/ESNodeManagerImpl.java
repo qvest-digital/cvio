aacf6a7542a60f9adb927ad06580439ce98a2f00
smancke <s.mancke@tarent.de>
2014-03-10 10:45:59 +0100

    corrected logging

--- src/main/java/org/tarent/cvio/server/common/ESNodeManagerImpl.java
+++ src/main/java/org/tarent/cvio/server/common/ESNodeManagerImpl.java
@@ -9,10 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * ES client manager creating a local es node.
- * Here we start an embedded elastic search server. 
- * For security reasons, this elasicsearch does not open
- * the http port, if not wanted.
+ * ES client manager creating a local es node. Here we start an embedded elastic
+ * search server. For security reasons, this elasicsearch does not open the http
+ * port, if not wanted.
  * 
  * @author smancke
  */
@@ -63,7 +62,7 @@ public class ESNodeManagerImpl implements ESNodeManager {
 
     @Override
     public void stop() throws Exception {
-        logger.info("starting new Elasticsearch cvio node with cluster name 'cvio'");
+        logger.info("stoping Elasticsearch");
         esNode.close();
     }
 
